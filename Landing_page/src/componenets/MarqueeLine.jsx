27cab54f4c2bad983894abbbfe9a92a554b80566
Ace <nox.ace001@gmail.com>
2025-03-18 00:07:57 +0530

    marquee line little change

--- Landing_page/src/componenets/MarqueeLine.jsx
+++ Landing_page/src/componenets/MarqueeLine.jsx
@@ -5,7 +5,7 @@ const Marquee = () => {
       
       <div className="rounded-2xl mx-14 mb-8 bg-black py-4 overflow-hidden whitespace-nowrap">
         <div className="marquee text-lg font-bold">
-          Welcome to SkEdutech! Start Your Journey With Us Now
+          Welcome to SkEdutech ! Start Your Journey With Us Now
         </div>
       </div>
     );
